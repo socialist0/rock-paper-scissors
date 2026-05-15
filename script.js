129150fe0f33cc19720ad1dcea1a930c804098a2
socialist0 <105346652+socialist0@users.noreply.github.com>
2026-05-15 13:37:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. Supabase 연결 설정 (변수명 통일)
-const SUPABASE_URL = 'https://zqocsmfeigllzqladkqj.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxb2NzbWZlaWdsbHpxbGFka3FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODcxNzYsImV4cCI6MjA5NDI2MzE3Nn0.RC6XmK9zSaX5BnXYz_-rUFu2YMOq4_pOw7qDPELdnIk';
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// script.js 상단
+const { createClient } = supabase;
+const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // 2. 게임 상태 변수
 let userScore = 0;
