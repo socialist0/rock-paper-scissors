72381572c11c136a84be3fe224bba94c48122e54
socialist0 <woorami@gmail.com>
2026-05-18 14:29:15 +0900

    Supabase 주소  수정

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 1. 내 프로젝트의 진짜 주소와 API 키
-const supabaseUrl = 'https://zqocsmfeigllyzqladkqj.supabase.co'; 
+const supabaseUrl = 'https://zqocsmfeigllzqladkqj.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxb2NzbWZlaWdsbHpxbGFka3FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODcxNzYsImV4cCI6MjA5NDI2MzE3Nn0.RC6XmK9zSaX5BnXYz_-rUFu2YMOq4_pOw7qDPELdnIk'; 
 
 // 2. 전역 변수로 _supabase 객체를 생성합니다 (가장 안전한 선언 방식)
