367a5b59055451723e3df92e55be71265082b151
socialist0 <woorami@gmail.com>
2026-05-18 14:10:52 +0900

    config.js수정함

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-const SUPABASE_URL = 'https://zqocsmfeigllzqladkqj.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxb2NzbWZlaWdsbHpxbGFka3FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODcxNzYsImV4cCI6MjA5NDI2MzE3Nn0.RC6XmK9zSaX5BnXYz_-rUFu2YMOq4_pOw7qDPELdnIk';
+// 주소와 키 설정은 그대로 두세요!
+const supabaseUrl = 'https://your-project-url.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+
+// 🌟 여기 맨 앞에 언더바(_)를 붙여서 '_supabase'로 바꿉니다!
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
