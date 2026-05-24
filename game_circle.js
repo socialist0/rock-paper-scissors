1ee9e808afbb395906ad111bba3555a039f9abf0
Sociallist0 <woorami@gmail.com>
2026-05-24 23:48:08 +0900

    fix: 점수부분 디자인 수정

--- game_circle.js
+++ game_circle.js
@@ -142,7 +142,7 @@ async function showMyRankNextToScore() {
 
             const rankSpan = document.createElement('span');
             rankSpan.id = 'my-rank-badge';
-            rankSpan.style.fontSize = '3rem';
+            rankSpan.style.fontSize = '2rem';
             rankSpan.style.fontWeight = 'bold';
             rankSpan.style.color = '#b45309';
             rankSpan.style.marginLeft = '16px';
