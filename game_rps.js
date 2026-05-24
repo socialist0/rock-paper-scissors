15057337657e43417bebf119de3d537143a131ee
Sociallist0 <woorami@gmail.com>
2026-05-25 00:30:08 +0900

    fix: 줄바꿈수수정

--- game_rps.js
+++ game_rps.js
@@ -14,7 +14,7 @@ async function playGame(userChoice) {
     let result = '';
 
     if (userChoice === computerChoice) {
-        result = `무승부입니다! 🤝\현재 ${rpsStreak}연승 유지 중!`;
+        result = `무승부입니다! 🤝 현재 ${rpsStreak}연승 유지 중!`;
         document.getElementById('result-text').innerText = 
             `나: ${koreanChoices[userChoice]} vs 컴퓨터: ${koreanChoices[computerChoice]}\n\n${result}`;
         return; 
@@ -26,7 +26,7 @@ async function playGame(userChoice) {
         (userChoice === 'scissors' && computerChoice === 'paper')
     ) {
         rpsStreak++;
-        result = `이겼습니다! 🎉\현재 ${rpsStreak}연승 중!`;
+        result = `이겼습니다! 🎉 현재 ${rpsStreak}연승 중!`;
         document.getElementById('user-score').innerText = rpsStreak;
         
     } else {
